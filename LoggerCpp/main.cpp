3c5ef30628ab9d87bef4c1d9423a89c4870ecccc
simant anand <simantanand20@gmail.com>
2024-05-01 15:31:29 +0530

    remove strace form Loggercpp/main.cpp

--- LoggerCpp/main.cpp
+++ LoggerCpp/main.cpp
@@ -3,7 +3,7 @@
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/stacktrace.hpp>
+
 
 using namespace std;
 namespace Log = boost::log;
